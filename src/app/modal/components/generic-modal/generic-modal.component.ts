4f2df3511307c3fa93d5bd084bc9b28364c13254
xhani-manolis-trungu <trmanolis@gmail.com>
2021-09-24 09:12:13 +0300

    1 file modified

--- src/app/modal/components/generic-modal/generic-modal.component.ts
+++ src/app/modal/components/generic-modal/generic-modal.component.ts
@@ -1,48 +1,65 @@
-import {Component, HostListener, OnInit, ViewChild} from '@angular/core';
-import {CdkPortalOutlet, PortalOutlet, TemplatePortal} from '@angular/cdk/portal';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import {
+  CdkPortalOutlet,
+  PortalOutlet,
+  TemplatePortal,
+} from '@angular/cdk/portal';
 
-import {ModalDirective} from 'ngx-bootstrap';
+import { ModalDirective } from 'ngx-bootstrap';
 
-import {GenericModalService} from '../../services/generic-modal.service';
-import {ModalPosition, ModalState} from '../../models';
+import { GenericModalService } from '../../services/generic-modal.service';
+import { ModalPosition, ModalState } from '../../models';
+
+export type KeyDownEvents =
+  | 'Enter'
+  | 'Escape'
+  | 'ArrowDown'
+  | 'ArrowUp'
+  | 'ArrowLeft'
+  | 'ArrowRight';
+
+export enum KeyDownEventsEnum {
+  'Enter' = 'Enter',
+  'Escape' = 'Escape',
+  'ArrowUp' = 'ArrowUp',
+  'ArrowRight' = 'ArrowRigth',
+  'ArrowDown' = 'ArrowDown',
+  'ArrowLeft' = 'ArrowLeft',
+}
 
 @Component({
   selector: 'app-generic-modal',
   templateUrl: './generic-modal.component.html',
-  styleUrls: ['./generic-modal.component.scss']
+  styleUrls: ['./generic-modal.component.scss'],
 })
 export class GenericModalComponent implements OnInit {
-
-  @ViewChild('modal', {static: false}) modal: ModalDirective;
-  @ViewChild(CdkPortalOutlet, {static: false}) portalOutlet: PortalOutlet;
+  @ViewChild('modal', { static: false }) modal: ModalDirective;
+  @ViewChild(CdkPortalOutlet, { static: false }) portalOutlet: PortalOutlet;
 
   modalPositionEnum = ModalPosition;
 
   private position: ModalPosition;
 
-  constructor(private genericModalService: GenericModalService) {
-  }
+  constructor(private genericModalService: GenericModalService) {}
 
   ngOnInit() {
-    this.genericModalService.state
-      .subscribe((state: ModalState) => {
-        if (!!state) {
-          if (state.open) {
-            this.modal.show();
-          } else {
-            this.modal.hide();
-          }
-          this.position = state.position || this.position;
+    this.genericModalService.state.subscribe((state: ModalState) => {
+      if (!!state) {
+        if (state.open) {
+          this.modal.show();
+        } else {
+          this.modal.hide();
         }
-      });
+        this.position = state.position || this.position;
+      }
+    });
 
-    this.genericModalService.portal
-      .subscribe((portal: TemplatePortal) => {
-        if (!!portal) {
-          this.portalOutlet.detach();
-          this.portalOutlet.attach(portal);
-        }
-      });
+    this.genericModalService.portal.subscribe((portal: TemplatePortal) => {
+      if (!!portal) {
+        this.portalOutlet.detach();
+        this.portalOutlet.attach(portal);
+      }
+    });
   }
 
   @HostListener('mousedown', ['$event'])
@@ -54,7 +71,8 @@ export class GenericModalComponent implements OnInit {
 
   @HostListener('keydown', ['$event'])
   onEsc(event: any) {
-    if (event.key === 'Escape') {
+    console.log(`Key event: `, event.key);
+    if ((event.key as KeyDownEvents) && KeyDownEventsEnum[event.key]) {
       this.genericModalService.close();
     }
   }
